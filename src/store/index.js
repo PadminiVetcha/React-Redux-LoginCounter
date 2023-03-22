d95ed186f22bffec2f271a12efbed1a5a065dfda
PadminiVetcha <vetchapaddu13@gmail.com>
2023-03-22 20:59:05 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import {configureStore } from '@reduxjs/toolkit';
+import authSlice from './auth';
+import counterSlice from './counter';
+
+
+
+//const store = createStore(counterSlice.reducer);
+const store = configureStore({
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+});
+export default store;
